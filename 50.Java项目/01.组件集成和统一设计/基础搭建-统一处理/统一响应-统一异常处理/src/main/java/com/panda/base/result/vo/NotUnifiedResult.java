f8a2a88af26aa4d7130d299ee2c677379754f5fa
guoshunfa <13301203454@163.com>
2023-07-05 13:56:31 +0800

    调整目录结构

--- 50.Java项目/01.组件集成和统一设计/基础搭建-统一处理/统一响应-统一异常处理/src/main/java/com/panda/base/result/vo/NotUnifiedResult.java
+++
@@ -1,9 +0,0 @@
-package com.panda.base.result.vo;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface NotUnifiedResult {
-}
--- 50.Java项目/01.组件集成和统一设计/集成-消息队列/集成kafka/README.md
+++
@@ -1,5 +0,0 @@
-# springboot项目集成kafka
-
-这里记录了两种引入的方法。
-1. 简单引入（panda-kafka-simple），功能只有发送和监听。
-2. 灵活引入（panda-kafka-control）。
